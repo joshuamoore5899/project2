128452cb68eead2dc60e4d6bd88cc7529db7b2f4
Josh <joshuamoore5899@gmail.com>
2021-02-26 23:12:04 -0700

    End Day 1

--- script.js
+++ script.js
@@ -52,8 +52,51 @@ function onClick(e) {
   });
 };
 
+function onClick2(e) {
+  e.preventDefault();
+  // get form values
+  let calories = document.getElementById('calories').value;
+  document.getElementById('result2').innerHTML = "";
+  //let s = document.getElementById('selector');
+  //let type = s.options[s.selectedIndex].value;
+
+  // check if number is empty
+  if (calories === null) {
+    calories = 2000;
+  }
+
+  // setup URL
+  let url3 = "https://api.spoonacular.com/mealplanner/generate?apiKey=1582ec83b96441669036ed721e4fd860&timeFrame=day&targetCalories=" + calories;
+  //let url = "http://numbersapi.com/" + number + "/" + type + "?json";
+  //let url = "http://numbersapi.com/random/year?json";
+  // call API
+  fetch(url3)
+    .then(function(response) {
+      // make sure the request was successful
+      if (response.status != 200) {
+        return {
+          text: "Error calling the Numbers API service: " + response.statusText
+        }
+      }
+      return response.json();
+    }).then(function(json) {
+      // update DOM with response
+        console.log(json);
+        for (let i = 0; i < json.length; i++) {
+          let plannerOutput = "<a href=" + json[i].sourceUrl + ">" + json[i].title + "</a>";
+          updateResult2(plannerOutput);
+        }
+      });
+    };
+
 function updateResult(info) {
   document.getElementById('result').innerHTML = document.getElementById('result').innerHTML + '<br><br>' + info;
 }
 
+function updateResult2(info) {
+  document.getElementById('result2').innerHTML = document.getElementById('result2').innerHTML + '<br><br>' + info;
+}
+
 document.getElementById('woo').addEventListener('click', onClick);
+
+document.getElementById('woo2').addEventListener('click', onClick2);
